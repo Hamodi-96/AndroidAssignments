14e37982abfc947d626a63d9b974ec8bd8eaa306
Hamodi-96 <ahmad_almshaiki96@hotmail.com>
2020-10-11 21:54:31 -0400

    part 1 is done

--- app/src/main/java/com/example/androidassignments/ListItemsActivity.java
+++ app/src/main/java/com/example/androidassignments/ListItemsActivity.java
@@ -1,14 +1,114 @@
 package com.example.androidassignments;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class ListItemsActivity extends AppCompatActivity {
-
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
+    ImageButton btnImage;
+    Switch btnSwitch;
+    CheckBox checkbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+        btnImage=findViewById(R.id.imageButton);
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,1);
+            }
+        });
     }
+    protected void onActivityResult(int requestCode, int responseCode, Intent data) {
+        super.onActivityResult(requestCode, responseCode, data);
+        if (requestCode == 1 && responseCode== RESULT_OK) {
+            Bundle extras=data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            btnImage.setImageBitmap(imageBitmap);
+        }
+
+    }
+    public void setOnCheckChanged(View v){
+        btnSwitch=findViewById(R.id.switchbtn);
+        CharSequence text = "Switch is On";// "Switch is Off"
+        int duration = Toast.LENGTH_SHORT; //= Toast.LENGTH_LONG if Off
+        if (btnSwitch.isChecked()==true){
+            text="Switch is On";
+            duration=Toast.LENGTH_SHORT;
+
+
+        }else if (btnSwitch.isChecked()==false){
+            text="Switch is Off";
+            duration=Toast.LENGTH_LONG;
+        }
+
+        Toast toast = Toast.makeText(this , text, duration); //this is the ListActivity
+        toast.show(); //display your message box
+
+    }
+    public void OnCheckChanged(View v){
+        checkbtn=findViewById(R.id.checkBox);
+        AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+// 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(R.string.dialog_message) //Add a dialog message to strings.xml
+
+                .setTitle(R.string.dialog_title)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                        Intent resultIntent = new Intent(  );
+                        resultIntent.putExtra("Response", "Here is my response");
+                        setResult(Activity.RESULT_OK, resultIntent);
+                        finish();
+
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                })
+                .show();
+
+
+    }
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+
+    }
+    protected void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+    protected void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
+
 }
--- app/src/main/java/com/example/androidassignments/LoginActivity.java
+++ app/src/main/java/com/example/androidassignments/LoginActivity.java
@@ -2,13 +2,63 @@ package com.example.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "LoginActivity";
+    SharedPreferences share;
+    Button login;
+    private EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+        login=findViewById(R.id.buttonLogIn);
+        email=findViewById(R.id.loginText);
+        share=getSharedPreferences("email", Context.MODE_PRIVATE);
+        email.setText(share.getString("emailLogIn", "abc1@Domain.com"));
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = share.edit();
+                editor.putString("emailLogIn", email.getText().toString());
+                editor.commit();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+    protected void onResume(){
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+
+    }
+    protected void onStart(){
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+    protected void onPause(){
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+    protected void onStop(){
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
